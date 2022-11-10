fc8100e212c97056403ec637734fc1b11041702a
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-10 07:52:30 -0300

    refactor: added port const

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import pokemonController from './controller/PokemonController';
 const app = express();
+const port = 8000;
 
-app.listen(8000, () => {
-    console.log(' eduzz está voando');
+app.listen(port, () => {
+    console.log(`A API está rodando na porta ${port}: http://localhost:${port}/`)
+    console.log('A EDUZZ está voando');
 });
 
 app.get('/', pokemonController.main);
